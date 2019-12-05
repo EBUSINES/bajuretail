ec346f87c057f89046da835ddbcae26e732b5e13
AlfalahAnugara <Alfalah190299@gmail.com>
2019-12-05 19:14:59 +0700

    create class add.php

---
+++ add.php
@@ -0,0 +1,45 @@
+<Html>
+<head>
+    <title>Add Customers</title>
+</head>
+
+<body>
+    <a href = "admin.php">Go to Home</a>
+    <br></br>
+
+    <form action="add.php" method="post" name="AddCustomers">
+        <table width = "25%" >
+            <tr>
+                <td>Nama Depan</td>
+                <td><input type = "text" name="nama_depan"></td>
+            </tr>
+
+            <tr>
+                <td>Nama Belakang</td>
+                <td><input type = "text" name="nama_belakang"></td>
+            </tr>
+
+            <tr>
+                <td>No Telepon</td>
+                <td><input type = "text" name="no_hp"></td>
+            </tr>
+
+            <tr>
+                <td>Alamat</td>
+                <td><input type = "text" name="alamat"></td>
+            </tr>
+
+            <tr>
+                <td>Provinsi</td>
+                <td><input type = "text" name="provinsi"></td>
+            </tr>
+
+            <tr>
+                <td>Kota</td>
+                <td><input type = "text" name="kota"></td>
+            </tr>
+
+            <tr>
+                <td>A/N</td>
+                <td><input type = "text" name="atas_nama"></td>
+            </tr>
