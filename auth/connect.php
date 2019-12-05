0834b59119f46ef9865e26561b0118036f84a43e
dzikrialifa <dzikrialif99@gmail.com>
2019-12-05 23:21:34 +0700

    Koneksi ke database

---
+++ auth/connect.php
@@ -0,0 +1,11 @@
+<?php 
+    $host       = "localhost";
+    $username   = "root";
+    $password   = "";
+    $db         = "";
+    $conn       = mysqli_connect("localhost","root","","bajuretail");
+
+    if ($conn) {
+        // echo "Connect";
+    }
+?>
