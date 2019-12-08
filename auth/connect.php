a877d816533552675dcb4f8044c6961b78b6d4c1
dzikrialifa <dzikrialif99@gmail.com>
2019-12-08 17:33:09 +0700

    Connect to Database

--- auth/connect.php
+++ auth/connect.php
@@ -1,7 +1,7 @@
 <?php 
     $conn       = mysqli_connect("localhost","root","","bajuretail");
-
     if ($conn) {
         // echo "Connect";
     }
+    define("BASE_PATH","http://localhost/bajuretail/style.css");
 ?>
