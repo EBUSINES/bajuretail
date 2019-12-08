dfc5539726de5a9a8db2f45c1cef36acd037ffcd
dzikrialifa <dzikrialif99@gmail.com>
2019-12-08 17:32:35 +0700

    Footer Website

--- footer.php
+++ footer.php
@@ -1,5 +1,29 @@
 <div class="container">
   <div class="footer">
+    <div class="row">
+        <div class="col-2 col-md-4">
+            <img src="https://images.unsplash.com/photo-1496200186974-4293800e2c20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="..." class="img-thumbnail logoku">
+        </div>
+        <div class="col-3 col-md-5">
+            <p><i class="fa fa-home"></i>Jalan Soekarno Hatta No 25 , Malang, Jawa Timur</p>
+            <p><i class="fa fa-phone"></i>0812-4332-2991</p>
+            <p><i class="fa fa-envelope"></i>helpme@bajuku.com</p>
+            <p><i class="fa fa-clock-o"></i>07.30 - 17.00</p>
+        </div>
+        <div class="col-6 col-md-3">
+            <form>
+                <div class="form-group">
+                    <label for="emails">Email address</label>
+                    <input type="email" class="form-control" id="emails" aria-describedby="emailHelp">
+                </div>
+                <div class="form-group">
+                    <label for="messages">Messages</label>
+                    <input type="password" class="form-control" id="messages">
+                </div>
+                <button type="submit" class="btn btn-primary btn-block btn-md"><i class="fa fa-send"></i> Send</button>
+                </form>
+        </div>
+    </div>
   </div>
 </div>
 <!-- Optional JavaScript -->
