6aed946948f954e2d648d1fffa772a5c3ff53390
dzikrialifa <dzikrialif99@gmail.com>
2019-12-12 22:54:40 +0700

    Item Class

--- Item.php
+++ Item.php
@@ -2,8 +2,9 @@
 
 class Item {
 
-    var $id;
-    var $nama_produk;
-    var $jumlah_produk;
-    var $harga;
+    var $id_produk; // id produk
+    var $nama_produk; // nama_produk
+    var $size_produk; // size_produk
+    var $jumlah_produk; // jumlah_produk
+    var $harga; // harga_produk
 }
