1bd24321502ce9b98e264ce0dc16ba1342ba7f3d
dzikrialifa <dzikrialif99@gmail.com>
2019-12-13 00:55:20 +0700

    Thanks Page

---
+++ view/thanks.php
@@ -0,0 +1,15 @@
+<?php 
+include '../header.php';
+?>
+<div class="container thanks">
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4"><b>Order Telah Diselesaikan</b> </h1>
+            <p class="lead">Terimakasih telah melakukan pembelian di <a href="<?= BASE_URL ?>">www.bajuretailku.com</a> , nantikan promo menarik lainnya hanya di website kami. Order yang telah diselesaikan akan diinformasikan melalui email.
+        Selamat berbelanja. </p>
+        </div>
+    </div>
+</div>
+<?php 
+include '../footer.php';
+?>
