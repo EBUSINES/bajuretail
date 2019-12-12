6e375fe6453cf6a3f1c02f516bf0de7af4a4074d
dzikrialifa <dzikrialif99@gmail.com>
2019-12-12 21:15:26 +0700

    Page Cara Pembayaran

---
+++ how-to-pay.php
@@ -0,0 +1,23 @@
+<?php 
+include "header.php";
+?>
+<div class="container pay"style="margin-top:8%;">
+    <h1 class="h1kat">Tata Cara Pembelian & Pembayaran</h1>
+    <nav>
+        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+            <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">1. Pilih Barang</a>
+            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">2. Checkout & Bayar</a>
+            <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">3. Konfirmasi</a>
+            <a class="nav-item nav-link" id="nav-barang-tab" data-toggle="tab" href="#nav-barang" role="tab" aria-controls="nav-barang" aria-selected="false">4. Pengiriman Barang</a>
+        </div>
+    </nav>
+    <div class="tab-content mt-3" id="nav-tabContent">
+        <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, quas??</div>
+        <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, veniam?</div>
+        <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur mollitia dignissimos tempore?</div>
+        <div class="tab-pane fade" id="nav-barang" role="tabpanel" aria-labelledby="nav-barang-tab">Lorem, ipsum dolor sit amet consectetur adipisicing.</div>
+    </div>
+</div>
+<?php 
+include "footer.php";
+?>
