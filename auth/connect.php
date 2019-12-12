a21f32644a5a24742138c6aa00f054cec0b1e5b1
dzikrialifa <dzikrialif99@gmail.com>
2019-12-12 21:13:15 +0700

    Connect Database

--- auth/connect.php
+++ auth/connect.php
@@ -3,5 +3,6 @@
     if ($conn) {
         // echo "Connect";
     }
-    define("BASE_PATH","http://localhost/bajuretail/style.css");
+    define("BASE_URL", "http://localhost/bajuretail");
+    define("BASE_CSS", "http://localhost/bajuretail/style.css");
 ?>
